c37b62cd6cb85f3690b83cddfdcd054c66fed8ab
Bradley Griffin <bigriff11@gmail.com>
2023-01-05 15:28:58 -0700

    Move all project2 elements to a new repo

--- index.js
+++ index.js
@@ -12,62 +12,33 @@ class Nav {
     document.querySelector(`.${target}`).remove();
   };
 
-  buildNav = (arr) => {
+  buildNav = () => {
+    let arr = [
+      ["nav", "content", ""],
+      ["navContainer", "nav", ""],
+      ["logo", "navContainer", "Du Bois"],
+      ["navItems", "navContainer", ""],
+      ["about", "navItems", "About"],
+      ["menu", "navItems", "Menu"],
+      ["bookNow", "navItems", "Book Now"],
+    ];
     for (let arg of arr) {
       this.build(arg[0], arg[1], arg[2]);
     }
   };
-  addTriangle = (arr) => {
+  addTriangle = (side) => {
+    let arr = [
+      [`triContainer${side}`, "navContainer", ""],
+      ["triangle", `triContainer${side}`, ""],
+    ];
     for (let arg of arr) {
       this.build(arg[0], arg[1], arg[2]);
     }
   };
 }
 
-let nav = new Nav();
-let arr = [
-  ["nav", "content", ""],
-  ["navContainer", "nav", ""],
-  ["logo", "navContainer", "Du Bois"],
-  ["navItems", "navContainer", ""],
-  ["about", "navItems", "About"],
-  ["menu", "navItems", "Menu"],
-  ["bookNow", "navItems", "Book Now"],
-];
-let arr2 = [
-  ["nav2", "content", ""],
-  ["navContainer2", "nav2", ""],
-  ["logo2", "navContainer2", "Du Bois"],
-  ["navItems2", "navContainer2", ""],
-  ["about2", "navItems2", "About"],
-  ["menu2", "navItems2", "Menu"],
-  ["bookNow2", "navItems2", "Book Now"],
-];
-let triCenter = [
-  ["triContainer", "navContainer", ""],
-  ["triangle", "triContainer", ""],
-];
-let triRight = [
-  ["triContainerRight", "navContainer", ""],
-  ["triangle", "triContainerRight", ""],
-];
-let triLeft = [
-  ["triContainerLeft", "navContainer", ""],
-  ["triangle", "triContainerLeft", ""],
-];
-let triCenter2 = [
-  ["triContainer2Menu", "nav2", ""],
-  ["triangle", "triContainer2Menu", ""],
-];
-let triRight2 = [
-  ["triContainer2Book", "nav2", ""],
-  ["triangle", "triContainer2Book", ""],
-];
-let triLeft2 = [
-  ["triContainer2About", "nav2", ""],
-  ["triangle", "triContainer2About", ""],
-];
-nav.buildNav(arr2);
-nav.addTriangle(triCenter2);
-nav.remove("triContainer2Menu");
-nav.addTriangle(triLeft2);
+// let nav = new Nav();
+// nav.buildNav();
+// nav.addTriangle("");
+// nav.remove("triContainer");
+// nav.addTriangle("Right");
